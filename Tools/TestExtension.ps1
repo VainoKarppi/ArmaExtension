a7b0979ac604eda527e44ec81526e315d749c431
Väinö Karppi <vaino.karppi@gmail.com>
2025-03-13 22:37:41 +0200

    UPDATED: Tester Tool

--- Tools/TestExtension.ps1
+++ Tools/TestExtension.ps1
@@ -10,9 +10,40 @@ if ($existingProcess) {
 
 # Check if the callExtension folder exists
 if (-Not (Test-Path -Path $callExtensionPath)) {
-    Write-Host "callExtension folder not found. Running DownloadExtensionTester.ps1..." -ForegroundColor Blue
-    # Run the DownloadExtensionTester script
-    & "$PSScriptRoot\DownloadExtensionTester.ps1"
+    Write-Host "callExtension folder not found. Running DownloadExtensionTester.ps1..." -ForegroundColor Green
+    
+    # Define the URL and the paths
+    $url = "http://killzonekid.com/pub/callExtension_v2.0.zip"
+    $zipPath = "$PSScriptRoot\callExtension_v2.0.zip"
+    $extractPath = "$PSScriptRoot\callExtension_v2.0"
+    $finalPath = "$PSScriptRoot\callExtension"
+
+    # Download the ZIP file
+    Invoke-WebRequest -Uri $url -OutFile $zipPath
+
+    # Extract the ZIP file
+    Expand-Archive -Path $zipPath -DestinationPath $extractPath
+
+    # Move the callExtension folder to the final path
+    Move-Item -Path "$extractPath\callExtension" -Destination $finalPath
+
+    # Remove the intermediate folder and ZIP file
+    Remove-Item -Path $extractPath -Recurse -Force
+    Remove-Item -Path $zipPath -Force
+
+    Write-Host "Download and extraction complete. The callExtension folder is ready at $finalPath." -ForegroundColor Blue
+
+    Write-Host "Removing unnecessary files..." -ForegroundColor Blue
+    # Remove specific files from the callExtension folder
+    $filesToRemove = @("readme.txt", "callExtension.exe", "test_callback.dll", "test_callback_x64.dll", "test_extension.dll", "test_extension_x64.dll", "test_script.sqf", "test_script2.sqf", "test_script3.sqf")
+    foreach ($file in $filesToRemove) {
+        $filePath = Join-Path -Path $finalPath -ChildPath $file
+        if (Test-Path -Path $filePath) {
+            Remove-Item -Path $filePath -Force
+        }
+    }
+
+    Write-Host "callExtension Downloaded Succesfully!" -ForegroundColor Green
 } else {
     Write-Host "callExtension folder found." -ForegroundColor Green
 }
@@ -120,7 +151,7 @@ if ($csprojFile) {
         }
 
         # Copy and Modify test.sqf file
-        Write-Output "Copying the tester script..."
+        Write-Host "Copying the tester script..." -ForegroundColor Blue
         $testScriptPath = "$destinationPath\current_test.sqf"
         Copy-Item -Path "$PSScriptRoot\base_test.sqf" -Destination "$testScriptPath" -Force
 
